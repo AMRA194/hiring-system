9911cc5918771db8f5d2fa30fc2879f32d5c4cc8
absar-glitch <juttabsar182@gmail.com>
2026-02-28 01:55:24 +0500

    Update home.php

--- home.php
+++ home.php
@@ -46,7 +46,7 @@ include 'components/add_cart.php';
          <div class="swiper-slide slide">
             <div class="content">
                <span>Project By</span>
-               <h3>Abdul Rehman</h3>
+               <h3>Amra</h3>
                <a href="menu.php" class="btn">Menu </a>
             </div>
             <div class="image">
