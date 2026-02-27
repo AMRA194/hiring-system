aa13d55207611f01e4920f413e92956266971233
Amra <samraimran@gmail.com>
2026-02-27 11:44:56 +0500

    first commit

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our Email</h3>
+         <a href="abdurrehman@gmail.com">abdurrehman@gmail.com</a>
+
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening Hours</h3>
+         <p>24/7</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our Address</h3>
+         <a href="#">University of Sialkot, Pakistan</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Our Number</h3>
+         <a href="tel:923416510355">+92 341 6510355</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; Copyright @ <?= date('Y'); ?> by <span>Abdul Rehman with Fellows</span> | All Rights Reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="/images/loader.gif" alt="">
+</div>
